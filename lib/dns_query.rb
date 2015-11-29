b281f4ca2f0bca3f111fa8b0190f0aa37b14cc3b
Jono Mingard <reason.koan@gmail.com>
2015-11-30 12:24:45 +1300

    Make request work for fixed query

--- lib/dns_query.rb
+++ lib/dns_query.rb
@@ -11,10 +11,10 @@ class DNSQuery
   def build
     header_bytes = [
       @id,
-      0, # none of the following 16 bits are required
+      0x0100, # query recursively
       @questions.size,
       0, 0, 0 # no other records
-    ].pack("S6")
+    ].pack("n6")
 
     question_bytes = @questions.map do |domain_name|
       parts = domain_name.split(".").map do |part|
@@ -23,7 +23,7 @@ class DNSQuery
 
       type = 0x01 #TODO support things other than A records
 
-      parts + 0.chr + [type].pack("S") + [0x0001].pack("S")
+      parts + 0.chr + [type].pack("n") + [0x0001].pack("n")
     end.join("")
 
     header_bytes + question_bytes
--- spec/dns_query_spec.rb
+++ spec/dns_query_spec.rb
@@ -4,7 +4,7 @@ describe DNSQuery do
   describe "#id" do
     it "becomes the first 16 bits in the header" do
       header = DNSQuery.new(123)
-      expect(header.build.unpack("S")[0]).to eq 123
+      expect(header.build.unpack("n")[0]).to eq 123
     end
   end
 
@@ -22,6 +22,24 @@ describe DNSQuery do
 
       expect(query.build[19].ord).to eq 3
       expect(query.build[20..22]).to eq "com"
+    end
+  end
+
+  describe "#build" do
+    let(:query) { DNSQuery.new(query_id) }
+    before { query.add_question(domain_name) }
+
+    context "with a query for google.com" do
+      let(:query_id) { 3 }
+      let(:domain_name) { "google.com" }
+
+      let(:expected_response) do
+        "\x00\x03\x01\x00\x00\x01\x00\x00\x00\x00\x00\x00\x06google\x03com\x00\x00\x01\x00\x01".force_encoding("ASCII-8BIT")
+      end
+
+      it "returns the expected query" do
+        expect(query.build).to eq expected_response
+      end
     end
   end
 end
